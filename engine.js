5c6fe96271dd75065afc5dc64f7ffc16a2d629ce
mikeswhitney33 <mikeswhitney33@gmail.com>
2022-09-27 21:10:02 -0400

    sets title of page now

--- engine.js
+++ engine.js
@@ -1,11 +1,16 @@
-
-
-
 class Game {
-    constructor(selector, title="Untitled Game") {
-        this.title = title;
+    constructor(selector, options) {
+        this.options = options;
         this.selector = selector;
         this.canvas = document.querySelector(selector);
         this.ctx = this.canvas.getContext("2d");
+    }
+
+    set_title() {
+        document.querySelector("title").innerHTML = this.options.title || "Untitled Game";
+    }
+
+    play_game() {
+        this.set_title();
     }
 }
